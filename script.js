3543d03d6178505b547519bee93839ad86b1c1dc
Ailla Maria Rodrigues Peguim <ailla.peguim@escola.pr.gov.br>
2026-07-07 20:39:07 -0300

    OK

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
-
-// Tamanho da fonte
-
+// Variável para armazenar o tamanho padrão da fonte
 let tamanho = 16;
 
+// Função para aumentar o tamanho da fonte da página
 document.getElementById("maior").onclick = function () {
     tamanho += 2;
     document.body.style.fontSize = tamanho + "px";
 };
 
+// Função para diminuir o tamanho da fonte da página
 document.getElementById("menor").onclick = function () {
     tamanho -= 2;
     document.body.style.fontSize = tamanho + "px";
 };
 
-// Curiosidades
-
+// Lista de dados contendo as curiosidades de cultivo indoor
 const lista = [
     "👩🏻‍💻 Tecnologia da NASA: a iluminação e irrigação indoor foram aprimoradas para alimentar astronautas.",
     "💧 Sistemas indoor podem economizar até 95% de água.",
@@ -23,6 +22,7 @@ const lista = [
     "🌦️ É possível produzir alimentos durante todo o ano."
 ];
 
+// Função para sortear e exibir uma curiosidade aleatória na tela
 document.getElementById("curiosidade").onclick = function () {
 
     const numero = Math.floor(Math.random() * lista.length);
@@ -31,8 +31,7 @@ document.getElementById("curiosidade").onclick = function () {
 
 };
 
-// Formulário
-
+// Função para processar o formulário e exibir a saudação personalizada com o nome
 document.getElementById("form").onsubmit = function (event) {
 
     event.preventDefault();
